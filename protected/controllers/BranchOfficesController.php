f75a8931e3d4615019be2960e8612c3ee8c02787
fergo21 <fernandoagodoy9@gmail.com>
2022-09-02 16:02:26 -0300

    fix name controller

--- protected/controllers/BranchOfficesController.php
+++ protected/controllers/BranchOfficesController.php
@@ -1,6 +1,6 @@
 <?php
 
-class BranchOfficesController extends Controller
+class BranchofficesController extends Controller
 {
 	/**
 	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
